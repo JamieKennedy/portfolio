3824f5049b4b4d32264bd1df2b0364b1eef65306
Jamie <jamiekennedy97@gmail.com>
2023-04-24 15:08:00 +0100

    Added about section contents

---
+++ src/components/ui/IconListItem.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { IconType } from 'react-icons';
+
+interface IIconListItemProps {
+    children: ReactNode;
+    className?: string;
+    icon: ReactNode;
+}
+
+const IconListItem: React.FunctionComponent<IIconListItemProps> = ({ icon, children, className }) => {
+    return (
+        <li className={`flex flex-row items-center${className}`}>
+            {icon}
+            {children}
+        </li>
+    );
+};
+
+export default IconListItem;
