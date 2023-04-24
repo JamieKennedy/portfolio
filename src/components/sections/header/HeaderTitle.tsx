35c477a664e106023e7efb6e99b45504107f833f
Jamie <jamiekennedy97@gmail.com>
2023-04-24 15:27:54 +0100

    Changed title logo

--- src/components/sections/header/HeaderTitle.tsx
+++ src/components/sections/header/HeaderTitle.tsx
@@ -4,8 +4,10 @@ const HeaderTitle = () => {
     return (
         <div className="flex w-8/12 grow flex-row items-end  leading-10 dark:text-white">
             <TransitionElement classBase="fadedown" duration={500}>
-                <div className="bg-white shadow-md dark:bg-slate-900">
-                    <h1 className="border-2 px-5 py-1 text-center font-rubik text-4xl font-medium italic">J K</h1>
+                <div className="relative flex w-32 flex-row border-2 bg-white py-1 shadow-md dark:bg-slate-900">
+                    <h1 className="mx-5 w-full font-rubik text-4xl font-medium italic">J</h1>
+                    <div className="absolute bottom-0 left-1/2 h-full w-1 -translate-x-1/2 -skew-x-12 bg-gray-300 dark:bg-white"></div>
+                    <h1 className="mx-5 w-full text-right font-rubik text-4xl font-medium italic">K</h1>
                 </div>
             </TransitionElement>
         </div>
